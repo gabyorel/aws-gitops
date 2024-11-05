48c8f4d34108be5dacc97843b206231ac705b77e
gabyorel <146829386+gabyorel@users.noreply.github.com>
2024-11-05 16:25:58 +0100

    configure terraform s3 backend

--- terraform/main.tf
+++ terraform/main.tf
@@ -6,9 +6,23 @@ terraform {
     }
   }
 
+  backend "s3" {
+    bucket = "gitops-tf-state-backend"
+    key    = "state/terraform.tfstate"
+    region = "us-east-1"
+  }
+
   required_version = ">= 1.2.0"
 }
 
 provider "aws" {
-  region  = "us-east-1"
+  region = "us-east-1"
+}
+
+resource "aws_s3_bucket" "terraform-state-backend" {
+  bucket = "gitops-tf-state-backend"
+
+  tags = {
+    Name = "Terraform State Backend"
+  }
 }
