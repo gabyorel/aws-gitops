ed3ef2f39debb15e3f5c01004c0b736390221cee
gabyorel <146829386+gabyorel@users.noreply.github.com>
2024-11-06 15:45:51 +0100

    feat: deploy new workload on webservers

--- terraform/scripts/nginx.sh
+++ terraform/scripts/nginx.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
 apt-get update
-apt-get install nginx -y
+apt-get install nginx git -y
+sudo rm -rf /var/www/html/*
+sudo git clone https://fhoubart@bitbucket.org/fhoubart/testphaser_aws.git /var/www/html/
+sudo mv /var/www/html/public_html/* /var/www/html/
