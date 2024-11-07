2281476a3767b670b402577b6e7e26725421868f
gabyorel <146829386+gabyorel@users.noreply.github.com>
2024-11-07 16:51:08 +0100

    feat: add ECR repository url output

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,3 +1,7 @@
 output "website_address" {
   value = "http://${module.webserver1.website_address}"
 }
+
+output "ecr_repository_url" {
+  value = var.global ? aws_ecr_repository.main[0].repository_url : ""
+}
