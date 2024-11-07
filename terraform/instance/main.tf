bb4ce39e66ed989da4540aac3ae164190267ffed
gabyorel <146829386+gabyorel@users.noreply.github.com>
2024-11-07 18:30:32 +0100

    refactor: use nginx template file instead of raw user data

--- terraform/instance/main.tf
+++ terraform/instance/main.tf
@@ -3,13 +3,11 @@ resource "aws_instance" "web" {
   instance_type          = var.instance_type
   subnet_id              = var.subnet_id
   vpc_security_group_ids = [var.security_group_id]
-  user_data              = <<EOF
-sudo apt-get update
-sudo apt-get install nginx git -y
-sudo rm -rf /var/www/html/*
-sudo git clone ${var.repo_url} /var/www/html/
-sudo mv /var/www/html/${var.html_folder}/* /var/www/html/
-EOF
+  user_data              = base64encode(templatefile(
+    "./instance/nginx.tftpl", {
+    repo_url=var.repo_url,
+    html_folder=var.html_folder
+  }))
 
   tags = {
     Name : "${var.instance_name}-${var.environment}"
