c7d9608c7fd68c8ec6a7e2fc154afbb6b8460b73
gabyorel <146829386+gabyorel@users.noreply.github.com>
2024-11-10 16:17:57 +0100

    chore: remove scripts folder and its content

--- terraform/scripts/nginx.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-apt-get update
-apt-get install nginx git -y
-sudo rm -rf /var/www/html/*
-sudo git clone https://fhoubart@bitbucket.org/fhoubart/testphaser_aws.git /var/www/html/
-sudo mv /var/www/html/public_html/* /var/www/html/
